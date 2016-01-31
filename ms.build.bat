cf568acf7f8f288ec3527437ddee455dbf741afa
Cy Scott <cy.a.scott@live.com>
2016-01-31 14:47:49 -0500

    making build scripts

---
+++ ms.build.bat
@@ -0,0 +1,2 @@
+src\ImageProcessor\.nuget\NuGet.exe restore src\ImageProcessor\ImageProcessor.sln
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=Release src\ImageProcessor\ImageProcessor.sln
